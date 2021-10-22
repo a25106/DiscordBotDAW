1f54bf4f83d473130d56728c992268d71a5c8a48
a25106 <92996598+a25106@users.noreply.github.com>
2021-10-22 16:52:37 +0200

    Add files via upload

---
+++ app/_Base-comandos.js
@@ -0,0 +1,11 @@
+  //Después de Alias es opcional.
+module.exports = { 
+    config: {
+        nombre: "",//Nombre del cmd
+        alias: [], //Alias
+        descripcion: "", //Descripción (OPCIONAL)
+        uso: ""//Su úso (OPCIONAL)
+    },
+  run: async (client, message, args) => {
+  }
+}
